983fb1a94ac47017f7518906d9bd66a721593dae
itsjackie00 <jackiedanso33@gmail.com>
2024-05-29 12:42:15 +0200

    added create comics

--- resources/views/comics/create.blade.php
+++ resources/views/comics/create.blade.php
@@ -2,35 +2,35 @@
 @extends('layouts.app')
 
 @section('content')
-    <h1>Create New Comic</h1>
+    <h1 class="m-3 text-primary">Create New Comic</h1>
     <form action="{{ route('comics.store') }}" method="POST">
         @csrf
         <div>
-            <label>Title</label>
+            <label class="form-label m-3">Title</label>
             <input type="text" name="title" value="{{ old('title') }}" required>
         </div>
         <div>
-            <label>Description</label>
+            <label class="form-label m-3">Description</label>
             <textarea name="description" required>{{ old('description') }}</textarea>
         </div>
         <div>
-            <label>Thumb</label>
+            <label class="form-label m-3">Thumb</label>
             <input type="text" name="thumb" value="{{ old('thumb') }}" required>
         </div>
         <div>
-            <label>Price</label>
+            <label class="form-label m-3">Price</label>
             <input type="text" name="price" value="{{ old('price') }}" required>
         </div>
         <div>
-            <label>Series</label>
+            <label class="form-label m-3">Series</label>
             <input type="text" name="series" value="{{ old('series') }}" required>
         </div>
         <div>
-            <label>Sale Date</label>
+            <label class="form-label m-3">Sale Date</label>
             <input type="date" name="sale_date" value="{{ old('sale_date') }}" required>
         </div>
         <div>
-            <label>Type</label>
+            <label class="form-label m-3">Type</label>
             <input type="text" name="type" value="{{ old('type') }}" required>
         </div>
         <button type="submit" class="btn btn-success">Save</button>
