30f8c363ff7a0d1704905968f56cd5ce38eb95db
itsjackie00 <jackiedanso33@gmail.com>
2024-05-29 10:53:42 +0200

    added functions

--- app/Http/Controllers/ComicController.php
+++ app/Http/Controllers/ComicController.php
@@ -14,8 +14,10 @@ class ComicController extends Controller
      */
     public function index()
     {
-        //
+        $comics = Comic::all();
+        return view('comics.index', compact('comics'));
     }
+    
 
     /**
      * Show the form for creating a new resource.
@@ -23,9 +25,9 @@ class ComicController extends Controller
      * @return \Illuminate\Http\Response
      */
     public function create()
-    {
-        //
-    }
+{
+    return view('comics.create');
+}
 
     /**
      * Store a newly created resource in storage.
@@ -34,9 +36,20 @@ class ComicController extends Controller
      * @return \Illuminate\Http\Response
      */
     public function store(Request $request)
-    {
-        //
-    }
+{
+    $data = $request->validate([
+        'title' => 'required|string|max:255',
+        'description' => 'required|string',
+        'thumb' => 'required|string|max:255',
+        'price' => 'required|numeric',
+        'series' => 'required|string|max:255',
+        'sale_date' => 'required|date',
+        'type' => 'required|string|max:255',
+    ]);
+
+    Comic::create($data);
+    return redirect()->route('comics.index');
+}
 
     /**
      * Display the specified resource.
@@ -45,9 +58,9 @@ class ComicController extends Controller
      * @return \Illuminate\Http\Response
      */
     public function show(Comic $comic)
-    {
-        //
-    }
+{
+    return view('comics.show', compact('comic'));
+}
 
     /**
      * Show the form for editing the specified resource.
@@ -56,9 +69,9 @@ class ComicController extends Controller
      * @return \Illuminate\Http\Response
      */
     public function edit(Comic $comic)
-    {
-        //
-    }
+{
+    return view('comics.edit', compact('comic'));
+}
 
     /**
      * Update the specified resource in storage.
@@ -69,7 +82,18 @@ class ComicController extends Controller
      */
     public function update(Request $request, Comic $comic)
     {
-        //
+        $data = $request->validate([
+            'title' => 'required|string|max:255',
+            'description' => 'required|string',
+            'thumb' => 'required|string|max:255',
+            'price' => 'required|numeric',
+            'series' => 'required|string|max:255',
+            'sale_date' => 'required|date',
+            'type' => 'required|string|max:255',
+        ]);
+    
+        $comic->update($data);
+        return redirect()->route('comics.index');
     }
 
     /**
@@ -79,7 +103,8 @@ class ComicController extends Controller
      * @return \Illuminate\Http\Response
      */
     public function destroy(Comic $comic)
-    {
-        //
-    }
+{
+    $comic->delete();
+    return redirect()->route('comics.index');
+}
 }
