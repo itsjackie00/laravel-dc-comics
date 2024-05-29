8710e54d67fab7d09e2d07bcddc3e1f239e7e3af
itsjackie00 <jackiedanso33@gmail.com>
2024-05-29 11:13:16 +0200

    created comic function run

--- database/seeders/ComicSeeder.php
+++ database/seeders/ComicSeeder.php
@@ -4,16 +4,27 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use App\Models\Comic;
 
 class ComicSeeder extends Seeder
 {
-    /**
-     * Run the database seeds.
-     *
-     * @return void
-     */
     public function run()
     {
-        //
+        $comics = [
+            [
+                'title' => 'Comic Title 1',
+                'description' => 'Description for Comic 1',
+                'thumb' => 'https://example.com/image1.jpg',
+                'price' => 9.99,
+                'series' => 'Series 1',
+                'sale_date' => '2023-01-01',
+                'type' => 'Type 1',
+            ],
+            // Aggiungi altri fumetti qui
+        ];
+
+        foreach ($comics as $comic) {
+            Comic::create($comic);
+        }
     }
 }
