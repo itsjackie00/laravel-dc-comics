b76de5d48da799e955f9a5690fdd2d91082cfaa2
itsjackie00 <jackiedanso33@gmail.com>
2024-05-31 10:26:47 +0200

    fix

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -5,19 +5,14 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Laravel DC Comics</title>
+    <!-- CSS -->
     <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+    <!-- Scss -->
+    <link rel="stylesheet" href="{{ asset('scss/app.scss') }}">
     <!-- Bootstrap -->
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css">
 </head>
 <body>
-    <header>
-        <nav>
-            <ul>
-                <li><a href="{{ route('comics.index') }}">Home</a></li>
-                <li><a href="{{ route('comics.create') }}">Create New Comic</a></li>
-            </ul>
-        </nav>
-    </header>
     <main class="container">
         @yield('content')
     </main>
