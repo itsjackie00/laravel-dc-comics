3606adc92e118e6179a5bbaae6e3656af9480c72
itsjackie00 <jackiedanso33@gmail.com>
2024-05-31 10:26:27 +0200

    added container and some margins

--- resources/views/comics/create.blade.php
+++ resources/views/comics/create.blade.php
@@ -2,47 +2,49 @@
 @extends('layouts.app')
 
 @section('content')
+<section class="container">
     <h1>Create New Comic</h1>
     @if ($errors->any())
-        <div>
-            <ul>
-                @foreach ($errors->all() as $error)
-                    <li>{{ $error }}</li>
-                @endforeach
-            </ul>
-        </div>
+    <div>
+        <ul>
+            @foreach ($errors->all() as $error)
+            <li>{{ $error }}</li>
+            @endforeach
+        </ul>
+    </div>
     @endif
     <form action="{{ route('comics.store') }}" method="POST">
         @csrf
-        <div>
+        <div class="m-3">
             <label>Title</label>
             <input type="text" name="title" value="{{ old('title') }}" required>
         </div>
-        <div>
+        <div class="m-3">
             <label>Description</label>
-            <textarea name="description" required>{{ old('description') }}</textarea>
+            <textarea name="description" cols="30" rows="5" required>{{ old('description') }}</textarea>
         </div>
-        <div>
+        <div class="m-3">
             <label>Thumb</label>
             <input type="text" name="thumb" value="{{ old('thumb') }}" required>
         </div>
-        <div>
+        <div class="m-3">
             <label>Price</label>
             <input type="text" name="price" value="{{ old('price') }}" required>
         </div>
-        <div>
+        <div class="m-3">
             <label>Series</label>
             <input type="text" name="series" value="{{ old('series') }}" required>
         </div>
-        <div>
+        <div class="m-3">
             <label>Sale Date</label>
             <input type="date" name="sale_date" value="{{ old('sale_date') }}" required>
         </div>
-        <div>
+        <div class="m-3">
             <label>Type</label>
             <input type="text" name="type" value="{{ old('type') }}" required>
         </div>
-        <button type="submit" class="btn btn-success">Save</button>
+        <button type="submit" class="btn btn-success m-2">Save</button>
     </form>
     <a href="{{ route('comics.index') }}" class="btn btn-secondary">Back to List</a>
+</section>
 @endsection
--- resources/views/comics/edit.blade.php
+++ resources/views/comics/edit.blade.php
@@ -2,6 +2,7 @@
 @extends('layouts.app')
 
 @section('content')
+<section class="container">
     <h1>Edit Comic</h1>
     @if ($errors->any())
         <div>
@@ -15,7 +16,7 @@
     <form action="{{ route('comics.update', $comic->id) }}" method="POST">
         @csrf
         @method('PUT')
-        <div>
+        <div class="m-3">
             <label>Title</label>
             <input type="text" name="title" value="{{ old('title', $comic->title) }}" required>
         </div>
@@ -23,27 +24,28 @@
             <label>Description</label>
             <textarea name="description" required>{{ old('description', $comic->description) }}</textarea>
         </div>
-        <div>
+        <div class="m-3">
             <label>Thumb</label>
             <input type="text" name="thumb" value="{{ old('thumb', $comic->thumb) }}" required>
         </div>
-        <div>
+        <div class="m-3">
             <label>Price</label>
             <input type="text" name="price" value="{{ old('price', $comic->price) }}" required>
         </div>
-        <div>
+        <div class="m-3">
             <label>Series</label>
             <input type="text" name="series" value="{{ old('series', $comic->series) }}" required>
         </div>
-        <div>
+        <div class="m-3">
             <label>Sale Date</label>
             <input type="date" name="sale_date" value="{{ old('sale_date', $comic->sale_date) }}" required>
         </div>
-        <div>
+        <div class="m-3">
             <label>Type</label>
             <input type="text" name="type" value="{{ old('type', $comic->type) }}" required>
         </div>
-        <button type="submit" class="btn btn-success">Save</button>
+        <button type="submit" class="btn btn-success m-2">Save</button>
     </form>
     <a href="{{ route('comics.index') }}" class="btn btn-secondary">Back to List</a>
+</section>
 @endsection
